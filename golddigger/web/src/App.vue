f6f2d996a2d11bf45314bc544d7aca1ce561b6b7
Andrew Robles <andrewrobles@verizon.net>
2023-11-28 18:48:16 -0800

    -m

--- golddigger/web/src/App.vue
+++ golddigger/web/src/App.vue
@@ -1,6 +1,13 @@
 <template>
 <router-link to="/1" class="link">Jamie Foxx</router-link>
 <router-link to="/2" class="link">Kanye West (Jamie Foxx)</router-link>
+<router-link to="/3" class="link">Kanye West</router-link><br>
+<router-link to="/4" class="link">Kanye West (Jamie Foxx)</router-link>
+<router-link to="/5" class="link">Kanye West</router-link>
+<router-link to="/6" class="link">Kanye West (Jamie Foxx)</router-link><br>
+<router-link to="/7" class="link">Kanye West</router-link>
+<router-link to="/8" class="link">Kanye West (Jamie Foxx)</router-link><br>
+<router-link to="/9" class="link">Kanye West</router-link>
 <br>
 <router-view></router-view>
 </template>
