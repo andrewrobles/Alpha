558898603111bb4a752ad313441c4686a01d6f98
Andrew Robles <andrewrobles@verizon.net>
2023-12-01 05:05:26 -0800

    Migrate Fast Lane

---
+++ web/lyrics/fastlane/Index.vue
@@ -0,0 +1,19 @@
+<template>
+<router-link to="/fastlane/1" class="link">Royce Da 5'9"</router-link>
+<router-link to="/fastlane/2" class="link">Eminem</router-link>
+<router-link to="/fastlane/hook" class="link">Hook</router-link>
+<router-link to="/fastlane/3" class="link">Royce Da 5'9"</router-link>
+<router-link to="/fastlane/4" class="link">Eminem</router-link>
+<router-link to="/fastlane/5" class="link">Royce</router-link>
+<router-link to="/fastlane/6" class="link">Eminem</router-link>
+<router-link to="/fastlane/hook" class="link">Hook</router-link>
+<br>
+<router-view></router-view>
+</template>
+    
+<style>
+.link {
+    margin-right: 10px	
+}
+</style>
+    
