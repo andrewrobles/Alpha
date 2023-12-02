02c43e29f448f04aa421e7bd9571eb363610567b
Andrew Robles <andrewrobles@verizon.net>
2023-12-02 08:26:49 -0800

    Rename psalms directory to books

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -1,15 +1,3 @@
-<!-- <script setup>
-import { onMounted } from 'vue'
-
-onMounted(() => {
-	console.log('test')
-	const url = 'https://andrewrobles.pythonanywhere.com/1'
-	const request = new Request(url, {method: 'GET'})
-	fetch(request).then(console.log(request))
-
-})
-</script> -->
-
 <template>
 <router-link to="/psalms" class="link">BOOK OF PSALMS</router-link><br>
 <router-view></router-view>
