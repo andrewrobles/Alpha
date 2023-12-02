6cf8f54a9f66fcc325c2d31249679c053afd4069
Andrew Robles <andrewrobles@verizon.net>
2023-12-02 09:38:29 -0800

    Decouple verses

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -3,29 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
 import PsalmsIndex from '../../vue/Index.vue'
-import Psalm1 from '../../psalms/1.vue'
-import Psalm2 from '../../psalms/2.vue'
-import Psalm3 from '../../psalms/3.vue'
-import Psalm4 from '../../psalms/4.vue'
-import Psalm5 from '../../psalms/5.vue'
-import Psalm6 from '../../psalms/6.vue'
-import Psalm7 from '../../psalms/7.vue'
-import Psalm8 from '../../psalms/8.vue'
-import Psalm9 from '../../psalms/9.vue'
-import Psalm10 from '../../psalms/10.vue'
+import Psalm from '../../vue/Psalm.vue'
 
 const routes = [
 	{ path: '/psalms', component: PsalmsIndex, name: 'PsalmsIndex' },
-	{ path: '/psalms/1', component: Psalm1, name: 'Psalm1' },
-	{ path: '/psalms/2', component: Psalm2, name: 'Psalm2' },
-	{ path: '/psalms/3', component: Psalm3, name: 'Psalm3' },
-	{ path: '/psalms/4', component: Psalm4, name: 'Psalm4' },
-	{ path: '/psalms/5', component: Psalm5, name: 'Psalm5' },
-	{ path: '/psalms/6', component: Psalm6, name: 'Psalm6' },
-	{ path: '/psalms/7', component: Psalm7, name: 'Psalm7' },
-	{ path: '/psalms/8', component: Psalm8, name: 'Psalm8' },
-	{ path: '/psalms/9', component: Psalm9, name: 'Psalm9' },
-	{ path: '/psalms/10', component: Psalm10, name: 'Psalm10' },
+	{ path: '/psalms/1', component: Psalm, name: 'Psalm1', props: { index: 1 }},
+	{ path: '/psalms/2', component: Psalm, name: 'Psalm2', props: { index: 2 }},
+	{ path: '/psalms/3', component: Psalm, name: 'Psalm3', props: { index: 3 }},
+	{ path: '/psalms/4', component: Psalm, name: 'Psalm4', props: { index: 4 }},
+	{ path: '/psalms/5', component: Psalm, name: 'Psalm5', props: { index: 5 }},
+	{ path: '/psalms/6', component: Psalm, name: 'Psalm6', props: { index: 6 }},
+	{ path: '/psalms/7', component: Psalm, name: 'Psalm7', props: { index: 7 }},
+	{ path: '/psalms/8', component: Psalm, name: 'Psalm8', props: { index: 8 }},
+	{ path: '/psalms/9', component: Psalm, name: 'Psalm9', props: { index: 9 }},
+	{ path: '/psalms/10', component: Psalm, name: 'Psalm10', props: { index: 10 }},
 ]
 
 const router = createRouter({
