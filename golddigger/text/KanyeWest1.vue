7c4628a62417b47829becd65e03c3c5c9416aa7c
Andrew Robles <andrewrobles@verizon.net>
2023-12-01 22:17:54 -0800

    Revert "Delete golddigger for now"
    This reverts commit 718e8c9c09008b5f2baf17c78fb019bdf711db17.

---
+++ golddigger/text/KanyeWest1.vue
@@ -0,0 +1,19 @@
+<template>
+Cutie the bomb, met her at a beauty salon<br>
+With a baby Louis Vuitton under her underarm<br>
+She said, "I can tell you rock, I can tell by your charm<br>
+Far as girls, you got a flock<br>
+I can tell by your charm and your arm"<br>
+But I'm lookin' for the one, have you seen her?<br>
+My psychic told me she'll have a ass like Serena<br>
+Trina, Jennifer Lopez, four kids<br>
+And I gotta take all they bad asses to ShowBiz?<br>
+Okay, get your kids, but then they got their friends<br>
+I pulled up in the Benz, they all got up in<br>
+We all went to din' and then I had to pay<br>
+If you fuckin' with this girl, then you better be paid<br>
+You know why? It take too much to touch her<br>
+From what I heard she got a baby by Busta<br>
+My best friend said she used to fuck with Usher<br>
+I don't care what none of y'all say, I still love her<br>
+</template>
