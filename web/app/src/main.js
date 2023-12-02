723338a31922a3c01c3cd83563b93d0c29d6355c
Andrew Robles <andrewrobles@verizon.net>
2023-12-02 08:51:12 -0800

    Change file naming convention

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -3,29 +3,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
 import PsalmsIndex from '../../vue/Index.vue'
-import PsalmOne from '../../psalms/PsalmOne.vue'
-import PsalmTwo from '../../psalms/PsalmTwo.vue'
-import PsalmThree from '../../psalms/PsalmThree.vue'
-import PsalmFour from '../../psalms/PsalmFour.vue'
-import PsalmFive from '../../psalms/PsalmFive.vue'
-import PsalmSix from '../../psalms/PsalmSix.vue'
-import PsalmSeven from '../../psalms/PsalmSeven.vue'
-import PsalmEight from '../../psalms/PsalmEight.vue'
-import PsalmNine from '../../psalms/PsalmNine.vue'
-import PsalmTen from '../../psalms/PsalmTen.vue'
+import Psalm1 from '../../psalms/Psalm1.vue'
+import Psalm2 from '../../psalms/Psalm2.vue'
+import Psalm3 from '../../psalms/Psalm3.vue'
+import Psalm4 from '../../psalms/Psalm4.vue'
+import Psalm5 from '../../psalms/Psalm5.vue'
+import Psalm6 from '../../psalms/Psalm6.vue'
+import Psalm7 from '../../psalms/Psalm7.vue'
+import Psalm8 from '../../psalms/Psalm8.vue'
+import Psalm9 from '../../psalms/Psalm9.vue'
+import Psalm10 from '../../psalms/Psalm10.vue'
 
 const routes = [
 	{ path: '/psalms', component: PsalmsIndex, name: 'PsalmsIndex' },
-	{ path: '/psalms/1', component: PsalmOne, name: 'PsalmOne' },
-	{ path: '/psalms/2', component: PsalmTwo, name: 'PsalmTwo' },
-	{ path: '/psalms/3', component: PsalmThree, name: 'PsalmThree' },
-	{ path: '/psalms/4', component: PsalmFour, name: 'PsalmFour' },
-	{ path: '/psalms/5', component: PsalmFive, name: 'PsalmFive' },
-	{ path: '/psalms/6', component: PsalmSix, name: 'PsalmSix' },
-	{ path: '/psalms/7', component: PsalmSeven, name: 'PsalmSeven' },
-	{ path: '/psalms/8', component: PsalmEight, name: 'PsalmEight' },
-	{ path: '/psalms/9', component: PsalmNine, name: 'PsalmNine' },
-	{ path: '/psalms/10', component: PsalmTen, name: 'PsalmTen' },
+	{ path: '/psalms/1', component: Psalm1, name: 'Psalm1' },
+	{ path: '/psalms/2', component: Psalm2, name: 'Psalm2' },
+	{ path: '/psalms/3', component: Psalm3, name: 'Psalm3' },
+	{ path: '/psalms/4', component: Psalm4, name: 'Psalm4' },
+	{ path: '/psalms/5', component: Psalm5, name: 'Psalm5' },
+	{ path: '/psalms/6', component: Psalm6, name: 'Psalm6' },
+	{ path: '/psalms/7', component: Psalm7, name: 'Psalm7' },
+	{ path: '/psalms/8', component: Psalm8, name: 'Psalm8' },
+	{ path: '/psalms/9', component: Psalm9, name: 'Psalm9' },
+	{ path: '/psalms/10', component: Psalm10, name: 'Psalm10' },
 ]
 
 const router = createRouter({
