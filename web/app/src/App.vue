56d9d7c99c1f59e90a07bf5d1332b22c0575ecd3
Andrew Robles <andrewrobles@verizon.net>
2023-12-01 23:19:37 -0800

    Add starter code for get request

--- web/app/src/App.vue
+++ web/app/src/App.vue
@@ -1,3 +1,15 @@
+<!-- <script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+	console.log('test')
+	const url = 'https://andrewrobles.pythonanywhere.com/1'
+	const request = new Request(url, {method: 'GET'})
+	fetch(request).then(console.log(request))
+
+})
+</script> -->
+
 <template>
 <router-link to="/psalms" class="link">Psalms</router-link><br>
 <router-link to="/fastlane" class="link">Fast Lane</router-link><br>
