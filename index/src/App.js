705f44a4eb0c4e39e47d27908a1f9863087d0cb1
Andrew Robles <andrewrobles@verizon.net>
2023-12-05 21:56:29 -0800

    Index

---
+++ index/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import Link from './components/Link'
+
+const links = require('./links.json')
+
+function App() {
+  return <Routes>
+      <Route path='/' element={ <></> }/>
+      {links.map(link => <Route path={link.path} element={ <Link url={link.url}/> }/>)}
+  </Routes>
+}
+
+export default App;
