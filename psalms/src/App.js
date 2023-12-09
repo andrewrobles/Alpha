a106353c4ef75550706f98a4b1f0d2735b2d9b49
Andrew Robles <andrewrobles@verizon.net>
2023-12-09 14:06:59 -0800

    feat(psalms): fetch psalm 1

--- psalms/src/App.js
+++ psalms/src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const BIBLE_ID = "de4e12af7f28f599-02";
+  const API_KEY = "4a203cf6d4603048c9008bc07df8be2a";
+  const URL = "https://api.scripture.api.bible/v1/bibles";
+  const getPsalmURL = (psalmIndex) => {
+    return `${URL}/${BIBLE_ID}/chapters/PSA.${psalmIndex}?content-type=text`;
+  };
+  const fetchData = async () => {
+    try {
+      const response = await fetch(getPsalmURL(1), {
+        method: "GET",
+        headers: {
+          "api-key": API_KEY,
+        },
+      });
+      const json = await response.json();
+      console.log(json.data.content);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
