7e4cafe3a6ebe6553cddd30631e09c9d3a553c58
Andrew Robles <andrewrobles@verizon.net>
2023-12-10 09:04:25 -0800

    psalms: define usepsalm hook

--- psalms/src/components/Chapter.js
+++ psalms/src/components/Chapter.js
@@ -1,26 +1,15 @@
 import { useEffect, useState } from "react";
+import api from '../api/index'
 
 const Chapter = () => {
+  const [text, setText] = useState('')
   useEffect(() => {
     fetchData();
   }, []);
-  const [text, setText] = useState("");
-  const BIBLE_ID = "de4e12af7f28f599-02";
-  const API_KEY = "4a203cf6d4603048c9008bc07df8be2a";
-  const URL = "https://api.scripture.api.bible/v1/bibles";
-  const getPsalmURL = (psalmIndex) => {
-    return `${URL}/${BIBLE_ID}/chapters/PSA.${psalmIndex}?content-type=text`;
-  };
   const fetchData = async () => {
     try {
-      const response = await fetch(getPsalmURL(1), {
-        method: "GET",
-        headers: {
-          "api-key": API_KEY,
-        },
-      });
-      const json = await response.json();
-      setText(json.data.content);
+      const content = await api.usePsalm(1)
+      setText(content);
     } catch (error) {
       console.error(error);
     }
