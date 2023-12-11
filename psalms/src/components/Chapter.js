4f398bf2732690d381ad95e99608347204801843
Andrew Robles <andrewrobles@verizon.net>
2023-12-11 06:58:16 -0800

    psalms: rename usechapter

--- psalms/src/components/Chapter.js
+++ psalms/src/components/Chapter.js
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
-import Link from '../components/Link'
-import api from '../api/index'
+import Link from "../components/Link";
+import api from "../api/index";
 
 const Chapter = () => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     try {
-      const content = await api.usePsalm(1)
+      const content = await api.useChapter(1);
       setText(content);
     } catch (error) {
       console.error(error);
     }
   };
-  return <div>
-    <Link href="/"/>
-    <div>{text}</div>
-  </div>;
+  return (
+    <div>
+      <Link href="/" />
+      <div>{text}</div>
+    </div>
+  );
 };
 
 export default Chapter;
