bbee0995474f36fbfcafef23cd1688acf7f78c9a
Andrew Robles <andrewrobles@verizon.net>
2023-12-12 14:52:16 -0800

    bible: delete project

--- psalms/src/api/index.js
+++ psalms/src/api/index.js
@@ -14,7 +14,7 @@ const useChapter = async (psalmIndex) => {
     });
     const json = await response.json();
     console.log(json);
-    return json.data.content;
+    return json?.data?.content;
   } catch (error) {
     console.error(error);
   }
