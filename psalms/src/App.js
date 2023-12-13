8515d5530c2b57babd2c44574d7c86a519e9452a
Andrew Robles <andrewrobles@verizon.net>
2023-12-13 01:49:08 -0800

    psalms: basiclist

--- psalms/src/App.js
+++ psalms/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Chapter from './components/Chapter'
+import BasicList from './components/BasicList'
 
 const App = () => {
   const arrayOfIndexes = [...Array(150).keys()];
@@ -9,6 +10,7 @@ const App = () => {
       <header className="App-header">
         <Router>
           <Routes>
+            <Route exact path="/list" element={<BasicList/>}/>
             <Route exact path="/" element={<Home/>}/>
             {arrayOfIndexes.map((value, index) => {
               return <Route exact path={`/${index + 1}`} element={<Chapter index={index + 1}/>}/>
