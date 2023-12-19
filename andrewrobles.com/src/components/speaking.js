cf28402b089524ddf2cbee89ba451d007dc720f3
Andrew Robles <andrewrobles@verizon.net>
2023-12-19 07:16:38 -0800

    ar speaking

--- andrewrobles.com/src/components/speaking.js
+++ andrewrobles.com/src/components/speaking.js
@@ -1,5 +1,24 @@
 const Speaking = () => {
-    return <div>Speaking</div>
-}
+  return (
+    <div>
+      <a href="/">home</a>
+      <h1>Hire Drew to speak at your next Event</h1>
+      <ul>
+        <li>Drew specializes in Q&A style talks by himself or on a panel</li>
+        <li>Optional addons:</li>
+        <ul>
+            <li>Drew can post about Q&A/event on TikTok</li>
+            <li>Ben can livestream the Q&A on Instagram</li>
+        </ul>
+        <li>Speaking fee:</li>
+        <ul>
+            <li>$2,000 for online</li>
+            <li>$6,000 for in person + travel expenses</li>
+        </ul>
+        <li>Get in touch: andrewrobles+speaking@berkeley.edu</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Speaking
+export default Speaking;
