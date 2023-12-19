4cc391ea39d26db9f00684f94970405ea0fa6462
Andrew Robles <andrewrobles@verizon.net>
2023-12-19 07:50:32 -0800

    ar links

--- andrewrobles.com/src/App.js
+++ andrewrobles.com/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import "./App.css";
 import Index from './components'
 import Speaking from './components/speaking'
+import Link from './components/Link'
+
+const links = require('./links.json')
 
 function App() {
   return <div>
@@ -9,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Index/>}/>
         <Route exact path="/speaking" element={<Speaking/>}/>
+        {links.map(link => <Route path={link.path} element={ <Link url={link.url}/> }/>)}
       </Routes>
     </Router>
   </div>;
