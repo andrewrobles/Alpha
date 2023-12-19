428eb9a9b9a37d864cbb7c632d22642f425501dd
Andrew Robles <andrewrobles@verizon.net>
2023-12-19 05:10:44 -0800

    drew jpg

--- andrewrobles.com/src/App.js
+++ andrewrobles.com/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Bio from './components/bio'
+import drew from './drew.jpg'
 
 function App() {
-  return <div><Bio/></div>;
+  return <div>
+    <img src={drew} width={175}/>
+    <Bio/>
+  </div>;
 }
 
 export default App;
