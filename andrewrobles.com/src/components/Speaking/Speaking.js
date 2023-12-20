27c2fc3c6d3e73c18b2c0a87795b8e796814b1ac
Andrew Robles <andrewrobles@verizon.net>
2023-12-19 16:30:01 -0800

    speaking link

--- andrewrobles.com/src/components/Speaking/Speaking.js
+++ andrewrobles.com/src/components/Speaking/Speaking.js
@@ -10,15 +10,42 @@ const Speaking = () => {
         <li>Drew specializes in Q&A style talks by himself or on a panel</li>
         <li>Optional addons:</li>
         <ul>
-          <li>Drew can post about Q&A/event on TikTok</li>
-          <li>Drew can livestream the Q&A on Instagram</li>
+          <li>
+            Drew can post about Q&A/event on{" "}
+            <Link
+              href="https://www.tiktok.com/@andrew.robles"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              TikTok
+            </Link>
+          </li>
+          <li>
+            Drew can livestream the Q&A on{" "}
+            <Link
+              href="https://www.instagram.com/andrewrobles_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </Link>
+          </li>
         </ul>
         <li>Speaking fee:</li>
         <ul>
-          <li>$2,000 for online</li>
-          <li>$6,000 for in person + travel expenses</li>
+          <li>$200 for online</li>
+          <li>$600 for in person + travel expenses</li>
         </ul>
-        <li>Get in touch: andrewrobles+speaking@berkeley.edu</li>
+        <li>
+          Get in touch:{" "}
+          <Link
+            href="mailto:andrewrobles+speaking@berkeley.edu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            andrewrobles+speaking@berkeley.edu
+          </Link>
+        </li>
       </ul>
     </div>
   );
