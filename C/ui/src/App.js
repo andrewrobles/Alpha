8b826a15135ed31dc469db041a82086bdc2f61b7
Andrew Robles <andrewrobles@verizon.net>
2023-12-28 14:24:47 -0800

    connect to firebase

--- C/ui/src/App.js
+++ C/ui/src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "./App.css";
 
 function App() {
+  // TODO: Replace the following with your app's Firebase project configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLRPzJR7eEOFwPlqZuEjFCeo5AWoMQvxU",
+    authDomain: "coffee-fec3b.firebaseapp.com",
+    projectId: "coffee-fec3b",
+    storageBucket: "coffee-fec3b.appspot.com",
+    messagingSenderId: "581575190040",
+    appId: "1:581575190040:web:718f13728c206d6458056d",
+    measurementId: "G-2FTTEGG9P5"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  useEffect(() => {
+    console.log("analytics:");
+    console.log(analytics)
+  });
+
   return (
     <div className="App">
       <header className="App-header">
