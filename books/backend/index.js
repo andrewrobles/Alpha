8c72435916bea7a2ea5ef95992aba593cd197c98
Andrew Robles <andrewrobles@verizon.net>
2024-01-07 04:24:15 -0800

    create book model

--- books/backend/index.js
+++ books/backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { PORT, mongoDBURL } from './config.js'
+import { Book } from './models/bookModel.js'
 
 const app = express()
 
@@ -8,6 +9,29 @@ app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome To MERN Stack')
 });
+
+// Route for Save a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (!request.body.title || ! request.body.author || ! request.body.publishYear ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            })
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        }
+
+        const book = await Book.create(newBook)
+
+        return response.status(201).send(newBook)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message})
+    }
+})
 
 mongoose.connect(mongoDBURL)
 .then(() => {
